4db873ca11e63c62c82cf89e60182a003a99b3e1
ssadichhya <sadichhya@fusemachines.com>
2023-11-02 11:20:25 +0545

    etl.py file edited

--- ETL/etl.py
+++ ETL/etl.py
@@ -11,34 +11,48 @@ yaml_file_path= 'config.yaml'
 with open(yaml_file_path, 'r') as yaml_file:
     config = yaml.safe_load(yaml_file)
 
+spark = SparkSession \
+    .builder \
+    .appName("final_project") \
+    .config("spark.jars", config['spark']['path']) \
+    .getOrCreate()
 
 # %%
 def extract(spark):
-        
-    #csv path
-    csv = config['csv']['path']
-    #Read raw_data
-    df = spark.read.csv(csv, header=True, inferSchema=False)
-    return df
+    try:
+        # CSV path
+        csv = config['csv']['path']
+        # Read raw_data
+        df = spark.read.csv(csv, header=True, inferSchema=False)
+        return df
+    except Exception as e:
+        raise Exception(f"An error occurred during data extraction: {str(e)}")
+        spark.stop()
 
 
 # %%
 def clean(spark):
-    df=extract(spark)
-    #changing -ve company size to positive
-    df = df.withColumn("Company Size", F.when(F.col("Company Size") < 0, -F.col("Company Size")).otherwise(F.col("Company Size")))
-    #List of columns to drop 
-    dropped = ["Contact Person", "Contact", "Benefits","Company Profile"]
-    #Drop column
-    df = df.drop(*dropped)
-    #changing to standard datatype
-    new_df = df.withColumn("Job Id", df["Job Id"].cast(LongType()))\
-        .withColumn("latitude", df["latitude"].cast(FloatType()))\
-         .withColumn("longitude", df["longitude"].cast(FloatType()))\
-          .withColumn("Company Size", df["Company Size"].cast(IntegerType()))\
-           .withColumn("Job Posting Date", df["Job Posting Date"].cast(DateType()))
-    return new_df
-
+    try:
+        df = extract(spark)
+        # Changing -ve company size to positive
+        df = df.withColumn("Company Size", F.when(F.col("Company Size") < 0, -F.col("Company Size")).otherwise(F.col("Company Size")))
+        
+        # List of columns to drop
+        dropped = ["Contact Person", "Contact", "Benefits", "Company Profile"]
+        # Drop columns
+        df = df.drop(*dropped)
+        
+        # Changing to standard data types
+        df = df.withColumn("Job Id", df["Job Id"].cast(LongType()))\
+               .withColumn("latitude", df["latitude"].cast(FloatType()))\
+               .withColumn("longitude", df["longitude"].cast(FloatType()))\
+               .withColumn("Company Size", df["Company Size"].cast(IntegerType()))\
+               .withColumn("Job Posting Date", df["Job Posting Date"].cast(DateType()))
+        
+        return df
+    except Exception as e:
+        raise Exception(f"An error occurred during data cleaning: {str(e)}")
+        spark.stop()
 
 # %%
 #udf to calculate avg
@@ -56,21 +70,69 @@ def calculate_average(range_str):
 
 # %%
 def transform(spark):
-    df=extract(spark)
-    calculate_average_udf = F.udf(calculate_average)
-    # Add a new column with the calculated average
-    new_df = df.withColumn("Average", calculate_average_udf(df["Salary Range"]))
-    new_df.withColumnRenamed("Average","Average Salary").printSchema()
-    return new_df
+    try:
+        df=clean(spark)
+        calculate_average_udf = F.udf(calculate_average)
+        # Add a new column with the calculated average
+        new_df = df.withColumn("Average", calculate_average_udf(df["Salary Range"]))
+        new_df = new_df.withColumnRenamed("Average","Average Salary")
+        new_df = new_df.withColumn("Average Salary",  
+                                  new_df["Average Salary"] 
+                                  .cast('int')) 
+
+        
+        # Changing gender preference from "both" to "Male or Female"
+        new_df = new_df.withColumn("Preference", F.when(F.col("Preference") == "Both", "Male or Female").otherwise(F.col("Preference")))
+        new_df.show()
+
+        #Dividing companies into tiers according to the company size
+
+        quartiles = new_df.stat.approxQuantile("company size", [0.25, 0.75], 0.0)
+        q1, q3 = quartiles
+        new_df = new_df.withColumn("CompanyTier",
+        F.when(F.col("company size") <= q1, "Tier-1 (Low)")
+        .when((F.col("company size") > q1) & (F.col("company size") <= q3), "Tier-2 (Medium)")
+        .when(F.col("company size") > q3, "Tier-3 (High)")
+        .otherwise("Uncategorized"))
+
+
+        #Dividing jobs into three categories according to their salary.
+        
+        quartiles = new_df.stat.approxQuantile("Average Salary", [0.25, 0.75], 0.0)
+        q1, q3 = quartiles
+        new_df = new_df.withColumn("SalaryLevel",
+        F.when(F.col("Average Salary") <= q1, "Low Pay")
+        .when((F.col("Average Salary") > q1) & (F.col("Average Salary") <= q3), "Average Pay")
+        .when(F.col("Average Salary") > q3, "High Pay")
+        .otherwise("Uncategorized"))
+        
+
+        # Differentiate qualifications according to their initials
+
+        new_df = new_df.withColumn("QualificationCategory", F.when(F.col("qualifications").startswith("M"), "Masters")
+        .when(F.col("qualifications").startswith("B"), "Bachelors")
+        .when(F.col("qualifications").startswith("P"), "PhD")
+        .otherwise("Uncategorized"))
+
+
+        return new_df
+
+    except Exception as e:
+        raise Exception(f"An error occurred during data transformation: {str(e)}")
+        spark.stop()
+
+
 
 # %%
 def load(spark):
-    new_df=transform(spark)
-    ##Load the clean data in postgres
-    new_df.write.format('jdbc').options(url=config['postgres']['url'],driver = config['postgres']['driver'], dbtable = config['postgres']['dbtable'], user=config['postgres']['user'],password=config['postgres']['password']).mode('overwrite').save()
-    return new_df
+    try:
+        dff=transform(spark)
+        ##Load the clean data in postgres
+        dff.write.format('jdbc').options(url=config['postgres']["url"],driver = config['postgres']["driver"], dbtable = config['postgres']["dbtable"], user=config['postgres']["user"],password=config['postgres']["password"]).mode('overwrite').save()
+        return dff
+    except Exception as e:
+        raise Exception(f"An error occurred during loading the data: {str(e)}")
+        spark.stop()    
 
-# %%
-# df.persist()
 
 
