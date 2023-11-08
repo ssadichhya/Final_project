a53879b02d1a97af8a1b537e4306707bb17fee57
ssadichhya <sadichhya@fusemachines.com>
2023-11-08 12:54:59 +0545

    main merged

--- ETL/ydata.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from ydata_profiling import ProfileReport
-
-csv = "/home/ubuntu/Desktop/final_project/Raw_Data/job_descriptions.csv"
-
-df = pd.read_csv(csv)
-profile = ProfileReport(df, title="Profiling Report")
